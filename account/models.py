47839c617e18f6f37d16d6eccc19adbe111c9b13
chintan7786 <davdachintan97@gmail.com>
2022-09-19 22:23:56 +0530

    created model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class User_Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    mobile = models.CharField(max_length=25)
+    otp = models.CharField(max_length=10)
+
+    
--- account/views.py
+++ account/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import User_Profile
 
 # Create your views here.
-def index(request):
+def register(request):
+    if request.method == 'POST':
+        pass
     return render(request, 'account/register.html')
